236124e0ff127dff080fbdc4c6d03cbe652ec960
Ziykon-Haykon <zike.haykon1x1@gmail.com>
2025-11-13 09:16:31 +0500

    Initial commit: add calc module, tests, and CI workflow

---
+++ test_calc.py
@@ -0,0 +1,12 @@
+from calc import add, div
+import pytest
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_div():
+    assert div(10, 2) == 5
+
+def test_div_zero():
+    with pytest.raises(ValueError):
+        div(1, 0)
